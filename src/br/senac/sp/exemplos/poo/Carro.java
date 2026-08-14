379c237144afbeb57437150fe378e5a3387f9fac
gabe <gquaresma57@gmail.com>
2026-08-14 10:56:03 -0300

    Hehehe

--- src/br/senac/sp/exemplos/poo/Carro.java
+++ src/br/senac/sp/exemplos/poo/Carro.java
@@ -7,9 +7,12 @@ public String chassi;
 private int velocidade;
 public void acelerar(){
     velocidade++;
+    System.out.printf("Velocidade: %d km/h%n", velocidade);
 
 }
 public void frear(){
     velocidade--;
+    System.out.printf("Velocidade: %d km/h%n", velocidade);
 }
+
 }
