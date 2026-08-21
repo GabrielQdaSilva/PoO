e8777f8be68a888d3b1748a7bf807aa070085d36
gabe <gquaresma57@gmail.com>
2026-08-21 11:13:05 -0300

    POO conta banco

--- src/br/senac/sp/ExemploContas.java
+++ src/br/senac/sp/ExemploContas.java
@@ -1,18 +1,10 @@
 package br.senac.sp;
 
+import br.senac.sp.banco.Cliente;
 import br.senac.sp.banco.Conta;
 
 public class ExemploContas {
-    static void main() {
-        Conta c1 = new Conta();
-        Conta c2 = new Conta();
-        Conta c3 = c1;
-
-        c1.depositar(1000);
-        c2.depositar(500);
-        c3.depositar(100);
-        c1.transferir(50, c2);
-        c3.transferir(100, c2);
+    static void main(String[] args) {
 
 
     }
